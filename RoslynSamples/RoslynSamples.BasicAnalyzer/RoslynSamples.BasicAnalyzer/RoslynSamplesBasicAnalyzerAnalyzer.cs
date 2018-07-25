fa5e9b4fd26b1e4b9e3457900815e13bb359eb8f
Mrnda <michal.mrnustik@outlook.com>
2018-07-25 14:30:09 +0200

    Change Make uppercase diagnostic to have maximal severity

--- RoslynSamples/RoslynSamples.BasicAnalyzer/RoslynSamples.BasicAnalyzer/RoslynSamplesBasicAnalyzerAnalyzer.cs
+++ RoslynSamples/RoslynSamples.BasicAnalyzer/RoslynSamples.BasicAnalyzer/RoslynSamplesBasicAnalyzerAnalyzer.cs
@@ -22,7 +22,7 @@ namespace RoslynSamples.BasicAnalyzer
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
         private const string Category = "Naming";
 
-        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
