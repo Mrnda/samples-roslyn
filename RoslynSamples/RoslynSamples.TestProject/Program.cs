3e61799e6c4c91ed321d5be3a5d61956ba2f80cb
Mrnda <michal.mrnustik@outlook.com>
2018-07-25 14:23:47 +0200

    Add testing project for analyzers

---
+++ RoslynSamples/RoslynSamples.TestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoslynSamples.TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
